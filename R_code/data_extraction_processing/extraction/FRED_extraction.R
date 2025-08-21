d0b96e81bca43fb14e832b811aa69407c90ab9d6
mle2718 <Min-Yang.Lee@noaa.gov>
2025-08-21 12:19:13 -0500

    remove hard-coded vintage. Extend to earlier years.  save into the proper place.

--- R_code/data_extraction_processing/extraction/FRED_extraction.R
+++ R_code/data_extraction_processing/extraction/FRED_extraction.R
@@ -4,7 +4,6 @@ library(here)
 library(tidyverse)
 library(glue)
 vintage_string<-format(Sys.Date())
-vintage_string<-"2025-06-18"
 # Make sure you have an API key and have set it in your .Renviron or .Rprofile 
 # If you have done this properly, you the following command should print your API key.
 
@@ -13,7 +12,7 @@ Sys.getenv("FRED_API_KEY")
 # Extract some data.
 deflators <- fredr(
   series_id = "GDPDEF",
-  observation_start = as.Date("2007-01-01"),
+  observation_start = as.Date("2000-01-01"),
   observation_end = as.Date("2025-01-01"),
   realtime_start =NULL,
   realtime_end =NULL,
@@ -24,4 +23,4 @@ deflators <- deflators %>%
   select(year, series_id, value) %>%
   arrange(year, series_id, value)
 
-write_rds(deflators, file=here("data_folder","main",glue("deflators_{vintage_string}.Rds")))
+write_rds(deflators, file=here("data_folder","raw",glue("deflators_{vintage_string}.Rds")))
